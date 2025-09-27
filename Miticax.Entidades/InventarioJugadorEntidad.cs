8a96c959903f0e103c090a900e9584af0842deba
kannoncoding <codingadaptations@gmail.com>
2025-09-27 13:51:36 -0600

    inventory added

---
+++ Miticax.Entidades/InventarioJugadorEntidad.cs
@@ -0,0 +1,21 @@
+﻿//UNED
+//Mitica X
+//Jorge Arias Melendez
+//Septiembre 2025
+//Entidad que representa una criatura dentro del inventario de un jugador (POCO).
+
+using System;
+
+namespace Miticax.Entidades
+{
+    public class InventarioJugadorEntidad
+    {
+        public int IdJugador { get; set; }
+
+        public int IdCriatura { get; set; }
+
+        public int Poder { get; set; }
+
+        public int Resistencia { get; set; }
+    }
+}
