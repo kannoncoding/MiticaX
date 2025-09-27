87af200c105e8c187c3f538299782345309c1b0e
kannoncoding <codingadaptations@gmail.com>
2025-09-27 13:50:41 -0600

    added player profile entity

---
+++ Miticax.Entidades/JugadorEntidad.cs
@@ -0,0 +1,25 @@
+﻿//UNED
+//Mitica X
+//Jorge Arias Melendez
+//Septiembre 2025
+//Entidad que representa un jugador del sistema (POCO; solo propiedades).
+
+using System;
+
+namespace Miticax.Entidades
+{
+    public class JugadorEntidad
+    {
+        public int IdJugador { get; set; }
+
+        public string Nombre { get; set; } = string.Empty;
+
+        public DateTime FechaNacimiento { get; set; }
+
+        public int NivelJugador { get; set; }
+
+        public int Cristales { get; set; } = 100;
+
+        public int BatallasGanadas { get; set; } = 0;
+    }
+}
